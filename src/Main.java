267220d184db2c7f3438b10a03cef4f89e40b201
Aryan Jaiswal <aryanjaiswal@Aryans-MacBook-Air.local>
2025-07-11 16:55:20 +0545

    CRUD Complete

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,59 @@ private static Statement statement = null;
         }
      
         
-   }   
+   } 
+   public static void updateData(String username, int userID) throws SQLException 
+   {
+       PreparedStatement preparedStatement=null;
+       ResultSet resultSet = null;
+       try{
+            connection = ConnectionClassConn.connectionClass();
+           String showDataQuery = "SELECT * FROM Student WHERE st_id =?";
+           preparedStatement = connection.prepareStatement(showDataQuery);
+           preparedStatement.setInt(1,userID);
+         resultSet =  preparedStatement.executeQuery();
+         if(resultSet.next())
+         {
+             String UpdateQuery = "UPDATE Student SET st_name = ? WHERE st_id =?";
+             preparedStatement = connection.prepareStatement(UpdateQuery);
+             preparedStatement.setString(1, username);
+             preparedStatement.setInt(2, userID);
+             int result = preparedStatement.executeUpdate();
+             System.out.println((result>0) ? "Database Updated" : "Not Updated");    
+         }
+         else{
+             System.out.println("Student doesnot exist in database");    
+            }
+         
+       }catch (SQLException sqlException){
+            System.out.println("Something went wrong while fetching student record.");
+            sqlException.printStackTrace();
+        }finally{
+           connection.close();
+        }
+ 
+   }
+   
+   public static void deleteData(int userID) throws SQLException
+   {
+       PreparedStatement preparedStatement = null;
+       try{
+        connection = ConnectionClassConn.connectionClass();
+        String deleteQuery ="DELETE FROM Student WHERE st_id = ?";
+        preparedStatement = connection.prepareStatement(deleteQuery);
+        preparedStatement.setInt(1, userID);
+      int result = preparedStatement.executeUpdate();
+           System.out.println((result>0)? "Deleted Successfully" : "Unsuccessfull");
+       } catch ( SQLException sqlException)
+       {
+            System.out.println("Something went wrong while fetching student record.");
+            sqlException.printStackTrace();
+       }
+       finally
+       {
+           connection.close();
+       }
+   }    
    
    public static void retrieveData()
    {
@@ -61,8 +113,17 @@ private static Statement statement = null;
         }
    }
 
-    public static void main(String[] args) {
-       insertIntoDatabase("7","aryan");
+    public static void main(String[] args) throws SQLException {
+       insertIntoDatabase("1","aryan");
+       insertIntoDatabase("2","sujata");
+       insertIntoDatabase("4","pratham");
+       insertIntoDatabase("5","jwala");
        retrieveData();
+        System.out.println("now updating");
+        updateData("bina",4);
+        System.out.println("Now deleting");
+        deleteData(5);
+        retrieveData();
+             
     }}
 
